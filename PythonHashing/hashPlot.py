f7695cab8d30e1085075796c63aa35b08b7d646f
Lennart Pries <lennartichbin@gmail.com>
2024-06-05 16:22:44 +0200

    Add Plot labels

--- PythonHashing/hashPlot.py
+++ PythonHashing/hashPlot.py
@@ -4,18 +4,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+
 def hashPlot(hashFunc, x, y):
     hashValues = np.array([hashFunc(x[i], y) for i in range(len(x))])
-    plt.plot(x, hashValues, 'ro')
+    plt.plot(x, hashValues, "ro")
+    plt.xlabel("Key")
+    plt.ylabel("Hash Value")
+    plt.title(hashFunc.__name__)
     plt.show()
-    
-    
+
+
 def hashFunc(x, y):
-    return x 
+    return x
+
 
 def hashByDivision(key, tableSize):
     return key % tableSize
-  
+
+
 def hashByMultiplication(key, tableSize):
     A = (math.sqrt(5) - 1) / 2
     return math.floor(tableSize * ((key * A) % 1))
@@ -23,10 +29,10 @@ def hashByMultiplication(key, tableSize):
 
 # 1000 Random values von 1 to 1000
 x = np.random.randint(1, 1000, 1000)
-# Hashtable size 
+# Hashtable size
 y = 100
 # x axis is the key values, y axis is the hash values
-# Plot the 3 hash functions 
+# Plot the 3 hash functions
 hashPlot(hashFunc, x, y)
 hashPlot(hashByDivision, x, y)
-hashPlot(hashByMultiplication, x, y)    
+hashPlot(hashByMultiplication, x, y)
